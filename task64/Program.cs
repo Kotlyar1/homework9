2c24f3abf8ceb76af55fc8a7d5decdc45a70a610
Александр Котляров <bony17@bk.ru>
2022-08-21 14:59:20 +0300

    решение 64 задачи

---
+++ task64/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+void ShowSequence(int start, int end)
+{
+    if (start > end)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(start + " ");
+        start++;
+        ShowSequence(start, end);
+    }
+}
+
+ShowSequence(m, n);
